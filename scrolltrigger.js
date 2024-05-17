1f0236afd70502ec05d0b818d4b5170d17fab660
Christian Albrechtsen <112468639+chri50nj@users.noreply.github.com>
2024-05-17 14:30:38 +0200

    Update scrolltrigger.js

--- scrolltrigger.js
+++ scrolltrigger.js
@@ -1,1 +1,30 @@
+$(".home-scroll_section").each(function (index) {
+  // kode her
+  let childTriggers = $(this).find(".home-scroll_text-item");
+  let childTargets = $(this).find(".home-scroll_img-item");
+  // skift aktiv klasse
+  function makeItemActive(index) {
+    childTriggers.removeClass("is-active");
+    childTargets.removeClass("is-active");
+    childTriggers.eq(index).addClass("is-active");
+    childTargets.eq(index).addClass("is-active");
+  }
+  makeItemActive(0);
+
+  // lav scrolltriggers
+  childTriggers.each(function (index) {
+    ScrollTrigger.create({
+      trigger: $(this),
+      start: "top center",
+      end: "bottom center",
+      markers: true
+
+      onToggle: (isActive) => {
+        if (isActive) {
+          makeItemActive(index);
+        }
+      },
+    });
+  });
+});
 
